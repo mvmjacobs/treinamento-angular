79a6989b38d1f70bdef84f169377b9fc072a62a0
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2018-02-14 08:59:26 -0200

    Exemplo de serviço

--- parte-4/service-app/src/app/app.component.ts
+++ parte-4/service-app/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+	@ViewChild('username') username: ElementRef;
+	users: any[] = [];
+	title = 'app';
+
+	constructor() { }
+
+	ngOnInit(): void {
+	}
+
+	addUser(username: string): void {
+		if (!username)
+			return;
+		this.users.push({ name: username, description: `Descrição do usuário ${username}` });
+		this.username.nativeElement.value = '';
+	}
 }
--- parte-4/service-app/src/app/app.module.ts
+++ parte-4/service-app/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		CommonModule
+	],
+	providers: [],
+	bootstrap: [
+		AppComponent
+	]
 })
 export class AppModule { }
