98bbb878085003a574138f7cedbf49319bdc5da3
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2018-02-14 22:30:12 -0200

    Adiciona Material no exemplo

--- parte-5/ng-forms/src/app/app.module.ts
+++ parte-5/ng-forms/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatOptionModule } from '@angular/material/core';
 
 import { AppComponent } from './app.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		MatFormFieldModule,
+		MatOptionModule,
+		MatSelectModule,
+		MatInputModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
