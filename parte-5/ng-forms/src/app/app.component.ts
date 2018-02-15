59415811b5d3372c686e354802b0f4e79e90c8b9
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2018-02-15 10:26:55 -0200

    Exemplos da ultima parte do treinamento

--- parte-5/ng-forms/src/app/app.component.ts
+++ parte-5/ng-forms/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ObservableMedia, MediaChange } from '@angular/flex-layout';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 	title = 'app';
+	value = 100;
+
+	subscribe: Subscription;
+	isMobile = false;
+
+	constructor(
+		private media: ObservableMedia
+	) { }
+
+	ngOnInit() {
+		this.subscribe = this.media.subscribe((change: MediaChange) => {
+			this.isMobile = change.mqAlias === 'sm' || change.mqAlias === 'xs';
+			console.log(this.isMobile);
+		});
+	}
 }
